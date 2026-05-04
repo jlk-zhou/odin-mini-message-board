32d1878cd97462be4f530ed16a0be90a8d2e7293
Zach Joe <jlk.zhou@gmail.com>
2026-05-04 11:58:19 +0900

    Add basic server-side data validations

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,13 +1,33 @@
-const db = require("../db/queries"); 
+const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const validateMessage = [
+  body("content")
+    .trim()
+    .isLength({ min: 1, max: 140 })
+    .withMessage(`Your message must be between 1 and 140 characters.`),
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .withMessage(`Your username must be between 1 and 30 characters.`),
+];
 
 function renderNewMessagePage(req, res) {
   res.render("new");
 }
 
-async function postNewMessage(req, res) {
-  const message = req.body; 
-  await db.createNewMessage(message); 
-  res.redirect("/");
-}
+const postNewMessage = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("new", { errors: errors.array() });
+    }
+
+    const { content, username } = matchedData(req);
+    await db.createNewMessage({ content, username });
+    res.redirect("/");
+  },
+];
 
 module.exports = { renderNewMessagePage, postNewMessage };
